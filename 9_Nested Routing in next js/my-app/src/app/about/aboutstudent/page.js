929e84234bc35074cbf4803c4a63217663e32d96
Tutul Kabir <tutulkabir@gmail.com>
2024-02-26 10:35:01 +0530

    nested routing

---
+++ 9_Nested Routing in next js/my-app/src/app/about/aboutstudent/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const AboutStudent = () => {
+  return (
+    <div>
+      <h1>About Page for Student</h1>
+      <Link href="/about">Go to About Page</Link>
+    </div>
+  );
+};
+
+export default AboutStudent;
---
+++ 9_Nested Routing in next js/my-app/src/app/about/page.js
@@ -0,0 +1,25 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+const About = () => {
+  const router = useRouter();
+  return (
+    <div>
+      <h1>About page</h1>
+      <Link href="/">Go to Home Page</Link>
+
+      <br />
+      <br />
+      <button onClick={() => router.push("/")}>Go to Home Page</button>
+      <br />
+      <br />
+      <Link href="/about/aboutcollege">Go to About College</Link>
+      <br />
+      <br />
+      <Link href="/about/aboutstudent">Go to About Student</Link>
+    </div>
+  );
+};
+
+export default About;

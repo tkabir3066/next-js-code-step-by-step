8ba3243659064740e3ad83c814a5f402ee11a304
Tutul Kabir <tutulkabir@gmail.com>
2024-03-01 18:49:29 +0530

    Export static HTML Page with build in next js

---
+++ 31_ Export Static HTML Page with Build in Next.js/my-app/src/app/seller/page.js
@@ -0,0 +1,11 @@
+import User from "@/components/User";
+import React from "react";
+
+export default function Seller() {
+  return (
+    <div>
+      <h1>Seller Page</h1>
+      <User />
+    </div>
+  );
+}

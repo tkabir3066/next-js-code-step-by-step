fde8348d77f3bbbee892dd56c5f8dbe5ffc7207d
Tutul Kabir <tutulkabir@gmail.com>
2024-03-04 10:15:29 +0530

    some changes

--- 36_GET API with Static Data in API Routes/next-api/src/app/api/users/[id]/route.js
+++ 36_GET API with Static Data in API Routes/next-api/src/app/api/users/[id]/route.js
@@ -1,9 +1,14 @@
-import { users } from "@/util/db";
+import { users } from "@/app/utils/db";
 import { NextResponse } from "next/server";
 
 export function GET(request, content) {
-  const data = users;
   console.log(content.params.id);
-  const userData = data.filter((user) => user.id == content.params.id);
-  return NextResponse.json(userData, { status: 200 });
+  const user = users.filter((user) => user.id == content.params.id);
+
+  return NextResponse.json(
+    user.length == 0
+      ? { result: "No data Found", success: false }
+      : { result: user, success: true },
+    { status: 200 }
+  );
 }
